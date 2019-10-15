17698a4389fd49701e320cf1349835686eeafa73
menglong45 <718322683@qq.com>
2019-10-15 10:00:12 +0800

    22

---
+++ 1234567.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+#denfine pi 3.1415926
+int main(void)
+{
+  double r;
+  printf("r=1");
+  scanf *("%if,&r");
+  printf ("球体的表面积为：%2if|n,4*pi*r*r");
+  printf (球体的体积为：%.2if\n,4.0/3*pi*r*r*r);
+  return 0;
+}
