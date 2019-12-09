ac4ae214aa705f81c2b3b0e646877fbc5549d817
menglong45 <718322683@qq.com>
2019-12-09 23:56:19 +0800

    ..

--- a1.c
+++ a1.c
@@ -5,15 +5,32 @@ w        ww        w
   w    w    w    w
    w  w      w  w
     ww        ww               */
+//  1.控制行数  2.控制输出w  3.控制输出空格  4控制换行符 //
 #include <stdio.h>
 int main(void)
 {
-    int i, j, k, n;
+    int i, j, k, n, b, c;
     printf("enter n:");
     scanf("%d", &n);
-    for (i = 1; i <= n; i++)
+    for (i = 1; i <= n; i++) //控制行数
     {
-        for (j = 1; j <= 2 * (n - 1); j++)
-            printf("*");
+        for (j = 0; j <= i - 1; j++)
+        {
+            printf(" ");
+        }
+        printf("w");
+        for (k = 1; k < 12 - j * 2; k++)
+            printf(" ");
+        printf("w");
+        for (b = 0; b <= 2*(i - 1); b++)
+        {
+            printf(" ");
+        }
+        printf("w");
+        for (c = 1; c <= 10 - b ; c++)
+            printf(" ");
+        printf("w");
+        printf("\n");
+    }
     return 0;
 }
