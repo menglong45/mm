eb1e0249f742abde52c850df18c73264c568f633
menglong45 <718322683@qq.com>
2019-12-13 09:01:29 +0800

    ..

---
+++ a7.c
@@ -0,0 +1,12 @@
+//输入10个数字并按从大到小的顺序排列。
+#include <stdio.h>
+int main(void)
+{
+    int i, n, a[n];
+    printf("enter n");
+    scanf("%d", n);
+    for (i = 1; i < n; i++)
+        scanf("%d", a[i]);
+
+    return 0;
+}
---
+++ a9.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define N 5
+int findmax(int *a, int n);
+int main(void)
+{
+    int a[5], i, max;
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    max = findmax(a, N);
+    printf("%d", max);
+}
+int findmax(int *a, int n)
+{
+    int i, max = 0;
+    for (i = 0; i < n; i++)
+    {
+        if (a[i] > a[max])
+        {
+            max = i;
+        }
+    }
+    return a[max];
+}
